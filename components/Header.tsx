218c4aa56fa6ac3bdcc47e7a923fd45854f655a8
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-05-06 10:16:57 +0200

    first commit posting working

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,12 +16,12 @@ const categories = [
 
 const Header = () => {
     return (
-        <div className='container mx-auto px-10 mb-8'>
+        <div className='container mx-auto px-10 mb-8 text-zinc-200'>
             <div className='border-b w-full inline-block border-blue-500 py-8'>
                 <div className='md:float-left block'>
                     <Link href='/'>
-                     <span className='cursor-pointer font-bold text-4xl text-white'>
-                         Bigl Blog
+                     <span className='cursor-pointer font-bold text-4xl'>
+                         Bigls Blog
                      </span>
                     </Link>
                 </div>
