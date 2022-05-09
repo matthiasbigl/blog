da3ecefbe925e32c943474e4b8435630d8c102f7
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-05-09 19:21:25 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images:{
-    domains: ['media.graphassets.com'],
+    domains: ['media.graphassets.com', 'https://sfkp.ch/assets/'],
+
+
     dangerouslyAllowSVG: true
   },
   reactStrictMode: true,
