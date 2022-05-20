3b5d20006ed39d4c93ab634cfb0ea8a2ecd66cd1
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-05-20 08:22:05 +0200

    lets See

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images:{
-    domains: ["media.graphassets.com", "sfkp.ch"],
+    async headers() {
+        return [
+            {
+                source: '/api/:slug',
+                headers: [
+                    {
+                        key: 'Authorization',
+                        value: process.env.GRAPHCMS_TOKEN,
+                    }
+                ]
+            }]
+    },
+    images: {
+        domains: ["media.graphassets.com", "sfkp.ch"],
+        source: '/blog/:slug',
 
 
-    dangerouslyAllowSVG: true
-  },
-  reactStrictMode: true,
+        dangerouslyAllowSVG: true
+    },
+    reactStrictMode: true,
 }
