65bc2b3d75eed7e13eb145b204e7f1732a3ec543
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-10-13 08:40:08 +0200

    added google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,19 @@ import type { AppProps } from 'next/app'
 import React, {useEffect,useState} from "react";
 import {Layout} from "../components";
 import {UserContext } from "../components/UserContext"
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
     return(
         // @ts-ignore
-      <UserContext.Provider value={"hello world from context"}>
-          <Layout>
-              <Component {...pageProps} />
-          </Layout>
-      </UserContext.Provider>
+        <>
+            // @ts-ignore
+            <Head>
+                <meta name="google-site-verification" content="AE4xTqjFEokU4TUV2f-qhOq_IDtXIsxOrGTKe15HNO4"/>
+            </Head><Layout>
+            <Component {...pageProps} />
+        </Layout></>
 
 
   );
